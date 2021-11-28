72dfdb186e58e0364283578a1f74ee8d92e3c088
Przemas91M <przemyslawiec@wp.eu>
2021-11-28 22:03:26 +0100

    Update joystick_atmega.ino

--- Projekty/Robot/joystick_atmega/joystick_atmega.ino
+++ Projekty/Robot/joystick_atmega/joystick_atmega.ino
@@ -1,35 +1,34 @@
-/*-----( Import needed libraries )-----*/
 #include<Wire.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-/*-----( Declare Constants and Pin Numbers )-----*/
+
 #define CE_PIN   9
 #define CSN_PIN 10
 #define JOYSTICK1_X A0
 #define JOYSTICK1_Y A1
 #define JOYSTICK2_Y A2
 #define JOYSTICK2_X A3
-// NOTE: the "LL" at the end of the constant is "LongLong" type
+
 const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe
-/*-----( Declare objects )-----*/
+
 RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
-/*-----( Declare Variables )-----*/
+
 int joystick[6];  // 6 element array holding Joystick readings
 int przycisk1;
 int przycisk2;
 int rozmiar;
 
-void setup()   /****** SETUP: RUNS ONCE ******/
+void setup()   
 {
   radio.begin();
   radio.openWritingPipe(pipe);
   pinMode(2, INPUT);
   pinMode(3, INPUT);
-}//--(end setup )---
+}
 
 
-void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+void loop()   
 {
   joystick[0] = analogRead(JOYSTICK1_X);
   joystick[1] = analogRead(JOYSTICK1_Y);
